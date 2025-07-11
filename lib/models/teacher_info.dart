33feb16b1fdb655276550d680a3a049d27414162
Ngoc Lan Le <voduykhanh3112@gmail.com>
2025-07-11 15:30:02 +0700

    vdkhanh update 11/07/2025

---
+++ lib/models/teacher_info.dart
@@ -0,0 +1,42 @@
+class TeacherInfo {
+  String id;
+  String user_id;
+  String attachments;
+  TeacherInfo({
+    required this.id,
+    required this.user_id,
+    required this.attachments,
+  });
+
+  factory TeacherInfo.initTeacherInfo() {
+    return TeacherInfo(
+      id: '',
+      user_id: '',
+      attachments: '',
+    );
+  }
+
+  static TeacherInfo fromJson(Map<String, dynamic> json) {
+    return TeacherInfo(
+      id: json['id'],
+      user_id: json['user_id'],
+      attachments: json['attachments'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'user_id': user_id,
+      'attachments': attachments,
+    };
+  }
+
+  Map<String, dynamic> toVal() {
+    return {
+      // 'id': id,
+      'user_id': user_id,
+      'attachments': attachments,
+    };
+  }
+}
